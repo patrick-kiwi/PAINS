22bc691a0cf7e0fbc17e52f6e4826c6bfd24b49b
Patrick O'Connor <patrick_kiwi@hotmail.com>
2015-08-10 09:50:15 +1200

    Update pfilter.pl

--- pfilter.pl
+++ pfilter.pl
@@ -3,14 +3,15 @@ use strict;
 use warnings;
 use Chemistry::File::SMARTS;
 use Chemistry::File::SDF;
+use Chemistry::Ring 'aromatize_mol';
 
 my $sdfFile = $ARGV[0];
 my @molecules = Chemistry::Mol->read("$sdfFile");
 
-my @painsFiles = qw/PAINS_FilterFamilyA.sieve 
-		    PAINS_FilterFamilyB.sieve
-		    PAINS_FilterFamilyC.sieve
+
+my @painsFiles = qw/PAINS.sieve
 		   /;
+
 
 my $pains;  	#Hash ref 'readable warning' => 'pattern object'
 my $painHits; 	#HoA_ref 'molecule_ID' => [ warnings ] 
@@ -19,16 +20,46 @@ my $painHits; 	#HoA_ref 'molecule_ID' => [ warnings ]
 
 loadPAINS(\@painsFiles); #load the PAINS to memory 
 searchMols(\@molecules); #run the PAINS search
+writeOutput(\@molecules); #write a new sdf file with the pains hits
+
+
+
+
+sub writeOutput {
+my $outfile = $sdfFile;
+$outfile =~ s/(.*)\.sdf/$1_PAINS\.sdf/;
+	foreach my $sdfMol ( @{$_[0]} ) {
+	my $snid = $sdfMol->attr("sdf/data")->{SNID};
+
+if (ref($sdfMol->attr("sdf/data")->{'comments'}) eq 'ARRAY' && $painHits->{$snid} ) {
+push (@{$sdfMol->attr("sdf/data")->{'comments'}}, @{$painHits->{$snid}})
+} else {
+$sdfMol->attr("sdf/data")->{'comments'} .= "\n$_" for (@{$painHits->{$snid}});
+}	
+					
+Chemistry::Mol->write( $outfile, mols => \@{$_[0]} );
+}
+}
+
+	 
 
 
 sub searchMols {
 	foreach my $sdfMol ( @{$_[0]} )  {
 	my $snid = $sdfMol->attr("sdf/data")->{SNID}; 	#Some unique SDF identifier.  In my case this is a field called SNID
-	next if ( $painHits->{$snid} );  #Skip if it's already been red-flagged
-		foreach my $warning ( keys %$pains ) {
-		if ( $pains->{$warning}->match($sdfMol) ) {
-		push ( @{$painHits->{$snid}}, $warning ); #red flat it!
-		print "Hit: ", $snid, " Warning: ", $warning, "\n";
+	#next if ( $painHits->{$snid} );  #Skip if it's already been red-flagged
+		foreach my $smartsString ( keys %$pains ) {
+			if ( $smartsString =~ /\p{L}/ ) {
+			aromatize_mol($sdfMol);
+				if ( $pains->{$smartsString}->[1]->match($sdfMol) ) {
+				push ( @{$painHits->{$snid}}, $pains->{$smartsString}->[0] ); #red flat it!
+				print "Hit: ", $snid, " Warning: ", $pains->{$smartsString}->[0], "\n";
+				}
+			} else {
+			if ( $pains->{$smartsString}->[1]->match($sdfMol) ) {
+				push ( @{$painHits->{$snid}}, $pains->{$smartsString}->[0] ); #red flat it!
+				print "Hit: ", $snid, " Warning: ", $pains->{$smartsString}->[0], "\n";
+			}
 	}
 }
 }
@@ -43,10 +74,12 @@ open (my $inFH, "<", $infile)
 while (<$inFH>) {
 next unless /^FRAGMENT\s+regId=(\S+)\s+(\S+)\s+/;
 my $readableWarning = $1;
-my $patternObject = Chemistry::Pattern->parse("$2", format => 'smarts' );
-$pains->{$readableWarning} = $patternObject;
+my $smartsString = $2;
+my $patternObject = Chemistry::Pattern->parse("$smartsString", format => 'smarts' );
+$pains->{$smartsString}->[0] = $readableWarning;
+$pains->{$smartsString}->[1] = $patternObject;
 }
 close $inFH;
 }
-print "*** Done loading PAIN filters ***\n";
+print "*** Done loading PAIN filters ***\n\n";
 }
